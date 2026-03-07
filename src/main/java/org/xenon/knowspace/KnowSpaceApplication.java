9add7a189b23658bd59a5f0ddaa7bae08532aa18
Winner <blessedwinner66@gmail.com>
2026-03-07 14:46:22 +0200

    Initial commit

---
+++ src/main/java/org/xenon/knowspace/KnowSpaceApplication.java
@@ -0,0 +1,13 @@
+package org.xenon.knowspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/xenon/knowspace/KnowSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xenon.knowspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
