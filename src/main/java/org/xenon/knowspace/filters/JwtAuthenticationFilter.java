6ca706e78528f9ac1afcaa42b5b892cb3522622a
Winner <blessedwinner66@gmail.com>
2026-03-10 20:52:23 +0200

    Done with jwt authentication filter

--- src/main/java/org/xenon/knowspace/filters/JwtAuthenticationFilter.java
+++ src/main/java/org/xenon/knowspace/filters/JwtAuthenticationFilter.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.xenon.knowspace.services.JwtService;
@@ -38,5 +40,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 null,
                 null
         );
+
+        authentication.setDetails(
+                new WebAuthenticationDetailsSource().buildDetails(request)
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(request,response);
     }
 }
