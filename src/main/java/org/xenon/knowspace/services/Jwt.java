49176b9c3cc8600cdf17f3addead467756185dc5
Winner <blessedwinner66@gmail.com>
2026-03-10 13:23:07 +0200

    Beginning with a jwt utility class

--- src/main/java/org/xenon/knowspace/services/Jwt.java
+++ src/main/java/org/xenon/knowspace/services/Jwt.java
@@ -2,6 +2,19 @@ package org.xenon.knowspace.services;
 
 import io.jsonwebtoken.Claims;
 
+import javax.crypto.SecretKey;
+import java.util.Date;
+
 public class Jwt {
-    private Claims claims;
+    private final Claims claims;
+    private final SecretKey secretKey;
+
+    public Jwt(Claims claims, SecretKey secretKey){
+        this.claims = claims;
+        this.secretKey = secretKey;
+    }
+
+    public boolean isExpired(){
+         return claims.getExpiration().before(new Date());
+    }
 }
