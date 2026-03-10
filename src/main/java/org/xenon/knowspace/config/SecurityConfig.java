2f4dc54af672a076165ebd92fc8b2eeb7fc32313
Winner <blessedwinner66@gmail.com>
2026-03-10 11:59:54 +0200

    Definedauthentication manager and password encoder beans

--- src/main/java/org/xenon/knowspace/config/SecurityConfig.java
+++ src/main/java/org/xenon/knowspace/config/SecurityConfig.java
@@ -1,11 +1,25 @@
 package org.xenon.knowspace.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 public class SecurityConfig {
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+        return config.getAuthenticationManager();
+    }
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
                 .sessionManagement(c-> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
