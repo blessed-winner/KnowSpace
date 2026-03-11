8e9733cf69c6f5d077c67fc0154b1d4782f745fa
Winner <blessedwinner66@gmail.com>
2026-03-11 16:18:07 +0200

    Custom utility function in user repository

--- src/main/java/org/xenon/knowspace/controllers/UserController.java
+++ src/main/java/org/xenon/knowspace/controllers/UserController.java
@@ -1,10 +1,13 @@
 package org.xenon.knowspace.controllers;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.xenon.knowspace.dtos.RegisterUserRequest;
 import org.xenon.knowspace.repositories.UserRepository;
 
 @RestController
@@ -14,7 +17,7 @@ public class UserController {
     private final UserRepository userRepository;
 
     @PostMapping
-    public ResponseEntity<?> register(){
+    public ResponseEntity<?> register(@Valid  @RequestBody RegisterUserRequest request){
 
     }
 }
