82255872cdd7973bd9d41108cbb1ff0a83b206ad
Winner <blessedwinner66@gmail.com>
2026-03-11 11:55:39 +0200

    Implemented the get me endpoint

--- src/main/java/org/xenon/knowspace/controllers/AuthController.java
+++ src/main/java/org/xenon/knowspace/controllers/AuthController.java
@@ -8,10 +8,13 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.xenon.knowspace.config.JwtConfig;
 import org.xenon.knowspace.dtos.JwtResponse;
 import org.xenon.knowspace.dtos.LoginRequest;
+import org.xenon.knowspace.dtos.UserDto;
+import org.xenon.knowspace.mappers.UserMapper;
 import org.xenon.knowspace.repositories.UserRepository;
 import org.xenon.knowspace.services.JwtService;
 
@@ -22,6 +25,7 @@ public class AuthController {
     private UserRepository userRepository;
     private final JwtService jwtService;
     private final JwtConfig jwtConfig;
+    private final UserMapper userMapper;
 
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(@Valid  @RequestBody LoginRequest request, HttpServletResponse response){
@@ -61,6 +65,19 @@ public class AuthController {
        return ResponseEntity.ok(new JwtResponse(accessToken.toString()));
     }
 
+    @GetMapping("/me")
+    public ResponseEntity<UserDto> me(){
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        var userId = (String) authentication.getPrincipal();
 
+        var user = userRepository.findById(userId).orElse(null);
+        if(user == null){
+            return ResponseEntity.notFound().build();
+        }
+
+        var userDto = userMapper.toDto(user);
+
+        return ResponseEntity.ok(userDto);
+    }
 
 }
