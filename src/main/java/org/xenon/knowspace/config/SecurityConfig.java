569dac9944e4698b9357628e9d76bb50b15e6783
Winner <blessedwinner66@gmail.com>
2026-03-12 14:01:15 +0200

    Authorized some routes for swagger access

--- src/main/java/org/xenon/knowspace/config/SecurityConfig.java
+++ src/main/java/org/xenon/knowspace/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         c->c.requestMatchers("/auth/**").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                                .requestMatchers("/swagger-ui.html", "/v3/api-docs/**, /swagger-ui/**").permitAll()
                                 .anyRequest().authenticated()
                 ).addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
