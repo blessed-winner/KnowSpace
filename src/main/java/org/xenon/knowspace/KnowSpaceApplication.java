9901e544621c2abae8f8095032d681609ca10f4a
Winner <blessedwinner66@gmail.com>
2026-03-12 13:28:24 +0200

    Removed unnecessary checks in the main function

--- src/main/java/org/xenon/knowspace/KnowSpaceApplication.java
+++ src/main/java/org/xenon/knowspace/KnowSpaceApplication.java
@@ -9,7 +9,6 @@ public class KnowSpaceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KnowSpaceApplication.class, args);
-        System.out.print(SpringBootVersion.getVersion());
     }
 
 }
