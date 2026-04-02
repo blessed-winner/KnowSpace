1ed3c202c813c7cd0ee4d048921412561d0c4995
Winner <blessedwinner66@gmail.com>
2026-04-02 13:21:43 +0200

    Addition of topics entity

--- src/main/java/org/xenon/knowspace/entities/Topic.java
+++ src/main/java/org/xenon/knowspace/entities/Topic.java
@@ -1,4 +1,24 @@
 package org.xenon.knowspace.entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "topics")
+@Data
 public class Topic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @Column(name = "creation_date")
+    private Date createdAt;
+
+    private String userid;
 }
