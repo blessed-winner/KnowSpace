65ed43ac271aecae8d40fb427b9851a371124157
Winner <blessedwinner66@gmail.com>
2026-04-04 14:05:39 +0200

    Addition of jwt response object

---
+++ src/main/java/org/xenon/knowspace/dtos/JwtResponse.java
@@ -0,0 +1,10 @@
+package org.xenon.knowspace.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JwtResponse {
+    private String token;
+}
--- src/main/java/org/xenon/knowspace/services/AuthService.java
+++ src/main/java/org/xenon/knowspace/services/AuthService.java
@@ -1,4 +1,8 @@
 package org.xenon.knowspace.services;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
 }
